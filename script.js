ed09de9195684e9e6b88429b1ea03e5a53084de0
JRPino <pino3789@gmail.com>
2020-03-19 15:16:00 -0700

    Assigned input value and local storage along with listing recent searches

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ $(document).ready(function () {
     //Event Listener to search button
     $(".button").on("click", function () {
         console.log("You got clicked!")
+
+        //Assigning the input a value & Storing it
+        citySearch = $("#city").val();
+        localStorage.setItem("recentCity", citySearch);
+        var recentSearch = localStorage.getItem("recentCity");
+        
+        // City Searched gets appended & listed to Recent Searches
+        $(city).text(name + citySearch);
+        $("#recent").append("<li>" + recentSearch + "</li>");
     })
 });
